2cd9f1f92614de9ce2027315f3ecdab4e784f154
laxesta <saracyan78@hotmail.com>
2022-05-16 00:15:20 +0200

    Add project files.

---
+++ Data/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPorDefectoComportamentoModificado.Data
+{
+    public class TodoItem
+    {
+        public string Titulo { get; set; }
+        public bool IsCompletado { get; set; }
+    }
+}
